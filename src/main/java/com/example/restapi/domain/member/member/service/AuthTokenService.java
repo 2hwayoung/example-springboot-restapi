0b4797646c4b90583dc0d2bdc2f27b575da07cf5
2hwayoung <hwa3270@gmail.com>
2025-02-07 14:37:28 +0900

    feat: generate access token service

--- src/main/java/com/example/restapi/domain/member/member/service/AuthTokenService.java
+++ src/main/java/com/example/restapi/domain/member/member/service/AuthTokenService.java
@@ -1,9 +1,24 @@
 package com.example.restapi.domain.member.member.service;
 
+import com.example.restapi.domain.member.member.entity.Member;
+import com.example.restapi.standard.util.Utils;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class AuthTokenService {
+
+    public String genAccessToken(Member member) {
+        int expireSeconds = 60 * 60 * 24 * 365;
+
+        return Utils.Jwt.createToken(
+                Keys.hmacShaKeyFor("abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes()),
+                expireSeconds,
+                Map.of("id", member.getId(), "username", member.getUsername())
+        );
+    }
 }
