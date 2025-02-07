5ad012c16177dbfdbed8b81ffa8a8f1dcd9cec28
2hwayoung <hwa3270@gmail.com>
2025-02-07 14:14:41 +0900

    feat: create Jwt Utils and test

---
+++ src/main/java/com/example/restapi/domain/member/member/service/AuthTokenService.java
@@ -0,0 +1,9 @@
+package com.example.restapi.domain.member.member.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthTokenService {
+}
--- src/main/java/com/example/restapi/standard/util/Utils.java
+++ src/main/java/com/example/restapi/standard/util/Utils.java
@@ -2,6 +2,12 @@ package com.example.restapi.standard.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 
 public class Utils {
     public static class Json {
@@ -13,6 +19,18 @@ public class Utils {
             } catch (JsonProcessingException e) {
                 throw new RuntimeException(e);
             }
+        }
+    }
+    public static class Jwt {
+        public static String createToken(Key secretKey, int expireSeconds, Map<String, Object> claims) {
+            Date issuedAt = new Date();
+            Date expiration = new Date(issuedAt.getTime() + 1000L * expireSeconds);
+            return Jwts.builder()
+                    .claims(claims)
+                    .issuedAt(issuedAt)
+                    .expiration(expiration)
+                    .signWith(secretKey) // SignatureAlgorithm.HS256
+                    .compact();
         }
     }
 }
